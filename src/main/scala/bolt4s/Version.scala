9b37f0392f0fe693445a29e27b11a0751d92a83f
Rutvik Patel <heyrutvik@gmail.com>
2021-01-04 01:00:39 +0530

    adds handshake

--- src/main/scala/bolt4s/Version.scala
+++ src/main/scala/bolt4s/Version.scala
@@ -9,6 +9,13 @@ final case class Version(major: Int, minor: Int)
 
 object Version {
 
+  lazy val `v3.0`: Version = Version(3, 0)
+  lazy val `v4.0`: Version = Version(4, 0)
+  lazy val `v4.1`: Version = Version(4, 1)
+  lazy val `v4.2`: Version = Version(4, 2)
+
+  lazy val versions = List(`v4.2`, `v4.1`, `v4.0`, `v3.0`)
+
   private val hex_00: Codec[Unit] = hex"00"
 
   implicit val codec: Codec[Version] = (hex_00 ~> hex_00 ~> uint8 ~ uint8).xmap(
